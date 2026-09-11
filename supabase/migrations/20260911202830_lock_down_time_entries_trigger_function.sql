f5e171c35f65e3db3daa8b4586b15a8ae47474b1
homreinigung-lang <homreinigung@gmail.com>
2026-09-11 22:28:55 +0200

    security: revoke RPC access to time entry integrity trigger

---
+++ supabase/migrations/20260911202830_lock_down_time_entries_trigger_function.sql
@@ -0,0 +1,3 @@
+REVOKE ALL ON FUNCTION public.time_entries_enforce_integrity() FROM PUBLIC;
+REVOKE ALL ON FUNCTION public.time_entries_enforce_integrity() FROM anon;
+REVOKE ALL ON FUNCTION public.time_entries_enforce_integrity() FROM authenticated;
